a67762f2b1b89fe993647fc35f24227a59c94d76
himanshu agarwal <himansha@andrew.cmu.edu>
2015-10-17 23:56:21 +1030

    changes for trial 2

--- app/src/androidTest/java/com/example/himanshu/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/himanshu/myapplication/ApplicationTest.java
@@ -10,5 +10,5 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-    //adding test comments
+    //adding test comments 2
 }
